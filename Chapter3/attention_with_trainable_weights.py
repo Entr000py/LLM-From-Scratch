b36b7e98ddd4c0a3b226d1b104a44606647ad334
jiangfei <j1310654263@gmail.com>
2025-07-11 13:59:33 +0800

    优化了自注意力机制示例代码

--- Chapter3/attention_with_trainable_weights.py
+++ Chapter3/attention_with_trainable_weights.py
@@ -35,59 +35,61 @@ class SelfAttention_v2(nn.Module):
     context_vec = attn_weights @ values
     return context_vec
 
-
-# 定义输入张量inputs，模拟6个词的嵌入向量，每个词由3个特征表示
-# 每个行向量代表一个词的嵌入，例如 "Your", "journey", "starts", "with", "one", "step"
 inputs = torch.tensor(
-  [[0.43, 0.15, 0.89], # Your     (x^1)
-   [0.55, 0.87, 0.66], # journey  (x^2)
-   [0.57, 0.85, 0.64], # starts   (x^3)
-   [0.22, 0.58, 0.33], # with     (x^4)
-   [0.77, 0.25, 0.10], # one      (x^5)
-   [0.05, 0.80, 0.55]] # step     (x^6)
-)
+    [[0.43, 0.15, 0.89], # Your     (x^1)
+    [0.55, 0.87, 0.66], # journey  (x^2)
+    [0.57, 0.85, 0.64], # starts   (x^3)
+    [0.22, 0.58, 0.33], # with     (x^4)
+    [0.77, 0.25, 0.10], # one      (x^5)
+    [0.05, 0.80, 0.55]] # step     (x^6)
+  )
 
-print(inputs) # 打印输入张量，查看其内容
+if __name__ == "__main__":
+  # 定义输入张量inputs，模拟6个词的嵌入向量，每个词由3个特征表示
+  # 每个行向量代表一个词的嵌入，例如 "Your", "journey", "starts", "with", "one", "step"
+  
 
-x_2 = inputs[1] # 获取输入张量中的第二个词向量 (journey)
-d_in = inputs.shape[1] # 获取输入特征的维度，即每个词向量的特征数量 (3)
-d_out = 2 # 定义输出特征的维度，即查询、键、值向量的维度 (2)
+  print(inputs) # 打印输入张量，查看其内容
 
-# torch.manual_seed(123) # 设置PyTorch的随机种子，以确保结果的可复现性
+  x_2 = inputs[1] # 获取输入张量中的第二个词向量 (journey)
+  d_in = inputs.shape[1] # 获取输入特征的维度，即每个词向量的特征数量 (3)
+  d_out = 2 # 定义输出特征的维度，即查询、键、值向量的维度 (2)
 
-# # 定义可训练的权重矩阵，用于将输入词向量转换为查询、键、值向量
-# # requires_grad=False 表示这些权重在训练过程中不会被优化（这里仅用于演示）
-# w_query = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False) # 查询权重矩阵
-# w_key = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
-# w_value = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False) 
+  # torch.manual_seed(123) # 设置PyTorch的随机种子，以确保结果的可复现性
 
-# # 计算第二个词向量 (x_2) 对应的查询、键、值向量
-# query_2 = x_2 @ w_query # x_2 与查询权重矩阵相乘，得到查询向量
-# key_2 = x_2 @ w_key     
-# value_2 = x_2 @ w_value 
+  # # 定义可训练的权重矩阵，用于将输入词向量转换为查询、键、值向量
+  # # requires_grad=False 表示这些权重在训练过程中不会被优化（这里仅用于演示）
+  # w_query = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False) # 查询权重矩阵
+  # w_key = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False)
+  # w_value = torch.nn.Parameter(torch.randn(d_in, d_out), requires_grad=False) 
 
-# # print(query_2) 
+  # # 计算第二个词向量 (x_2) 对应的查询、键、值向量
+  # query_2 = x_2 @ w_query # x_2 与查询权重矩阵相乘，得到查询向量
+  # key_2 = x_2 @ w_key     
+  # value_2 = x_2 @ w_value 
+
+  # # print(query_2) 
 
 
-# keys = inputs @ w_key   # 所有输入词向量与键权重矩阵相乘，得到所有键向量
-# values = inputs @ w_value 
+  # keys = inputs @ w_key   # 所有输入词向量与键权重矩阵相乘，得到所有键向量
+  # values = inputs @ w_value 
 
-# print("values.shape", values.shape) 
-# print("keys.shape", keys.shape)    
+  # print("values.shape", values.shape) 
+  # print("keys.shape", keys.shape)    
 
-# #注意力得分
-# keys_2 = keys[1]
-# attn_score_2 = query_2.dot(keys_2)
-# print("attn_score_22", attn_score_2)
+  # #注意力得分
+  # keys_2 = keys[1]
+  # attn_score_2 = query_2.dot(keys_2)
+  # print("attn_score_22", attn_score_2)
 
-# d_k = keys.shape[-1]
-# atten_weights_2 = torch.softmax(attn_score_2 / d_k**0.5, dim=-1)
-# print(atten_weights_2)
+  # d_k = keys.shape[-1]
+  # atten_weights_2 = torch.softmax(attn_score_2 / d_k**0.5, dim=-1)
+  # print(atten_weights_2)
 
-# context_vec_2 = attn_weights_2 @ values
-# print(context_vec_2)
+  # context_vec_2 = attn_weights_2 @ values
+  # print(context_vec_2)
 
-#举例
-torch.manual_seed(123)
-sa_v1 = self_attention_v1(d_in, d_out)
-print(sa_v1(inputs))
+  #举例
+  torch.manual_seed(123)
+  sa_v1 = self_attention_v1(d_in, d_out)
+  print(sa_v1(inputs))
